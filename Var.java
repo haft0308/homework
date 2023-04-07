fe190ff26655298ebda8c59795a6a3dd7d9280c1
haft0308 <66650268+haft0308@users.noreply.github.com>
2023-04-07 15:24:11 +0900

    Add files via upload
    23.04. 06 변수 예제

---
+++ Var.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class Var {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		
+		//정수 10을 담은 변수 num을 선언하고, 값을 할당하자
+		//그 뒤에 num값을 출력하자. 
+		
+		//1. 변수선언
+		//2. 변수에 값 할당
+		//3. 변수 값 출력
+		
+		//boolean 데이터 타입 변수 . false를 값할당해보자
+		//1. 변수선언
+				//2. 변수에 값 할당
+				//3. 변수 값 출력
+		
+		int temp=0x1f;
+		System.out.printf("%x, %d, %o", temp, temp, temp);
+	}
+	
+}
+
